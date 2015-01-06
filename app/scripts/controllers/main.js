2674a212a2dafd6cc9a453e92787900de4eb5186
Prashant Khanduri <pkhanduri@jawbone.com>
2015-01-05 21:07:56 -0800

    angular setup with ng-repeat

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,22 @@
  */
 angular.module('petstoreApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    $scope.pets = [
+      {
+        name: 'Sprout',
+        history: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        image: 'images/pet_01.jpg',
+        reviews: []
+      }, {
+        name: 'Oliver',
+        history: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        image: 'images/pet_02.jpg',
+        reviews: []
+      }, {
+        name: 'Doc',
+        history: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        image: 'images/pet_03.jpg',
+        reviews: []
+      }
     ];
   });

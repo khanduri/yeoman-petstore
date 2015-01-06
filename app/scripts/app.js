a9987ca5100048dee3575ffd737dfe82e9233ac1
Prashant Khanduri <pkhanduri@jawbone.com>
2015-01-05 16:50:35 -0800

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name petstoreApp
+ * @description
+ * # petstoreApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('petstoreApp', []);
